8140ffb896fd0faa75ac2c1cd2a82faa952cd02b
Conan Bitter <conan.bitter@gmail.com>
2024-10-20 18:47:18 +0300

    Try ndarray

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{f64::consts::PI, path::Path};
 
 use anyhow::Result;
 use image::{ImageBuffer, ImageReader, Luma, Rgb};
+use ndarray::{Array, Array2, ShapeBuilder};
+use ndarray_stats::QuantileExt;
 use once_cell::sync::Lazy;
 
 type Block = [f64; 8 * 8];
@@ -46,37 +48,34 @@ fn yuv2rgb(y: f64, u: f64, v: f64) -> (u8, u8, u8) {
     );
 }
 
-fn load_planes<P: AsRef<Path>>(filename: P, yp: &mut Vec<f64>, up: &mut Vec<f64>, vp: &mut Vec<f64>) -> Result<()> {
+fn load_planes<P: AsRef<Path>>(
+    filename: P,
+    yp: &mut Array2<f64>,
+    up: &mut Array2<f64>,
+    vp: &mut Array2<f64>,
+) -> Result<()> {
     let img = ImageReader::open(filename)?.decode()?.to_rgb8();
 
-    let image_width = img.width();
-    let image_height = img.height();
-    let y_plane_width = (image_width as f64 / 16.0).ceil() as u32 * 16;
-    let y_plane_height = (image_height as f64 / 16.0).ceil() as u32 * 16;
-    let uv_plane_width = y_plane_width / 2;
-    let uv_plane_height = y_plane_height / 2;
+    let image_width = img.width() as usize;
+    let image_height = img.height() as usize;
+    let y_plane_width = (image_width as f64 / 16.0).ceil() as usize * 16;
+    let y_plane_height = (image_height as f64 / 16.0).ceil() as usize * 16;
 
     for py in 0..y_plane_height {
         for px in 0..y_plane_width {
-            let ix = px.min(image_width - 1);
-            let iy = py.min(image_height - 1);
+            let ix = px.min(image_width - 1) as u32;
+            let iy = py.min(image_height - 1) as u32;
             let Rgb([r, g, b]) = *img.get_pixel(ix, iy);
 
-            let y_index = (px + py * y_plane_width) as usize;
-            let uv_index = (px / 2 + py / 2 * uv_plane_width) as usize;
             let (y, u, v) = rgb2yuv(r, g, b);
-            yp[y_index] = y;
-            up[uv_index] += u;
-            vp[uv_index] += v;
+            yp[(px, py)] = y;
+            up[(px / 2, py / 2)] += u;
+            vp[(px / 2, py / 2)] += v;
         }
     }
 
-    for p in up.iter_mut() {
-        *p /= 4.0;
-    }
-    for p in vp.iter_mut() {
-        *p /= 4.0;
-    }
+    *up /= 4.0;
+    *vp /= 4.0;
 
     return Ok(());
 }
@@ -84,10 +83,10 @@ fn load_planes<P: AsRef<Path>>(filename: P, yp: &mut Vec<f64>, up: &mut Vec<f64>
 fn main() -> Result<()> {
     let img = ImageReader::open("data/vid/test10/001.tif")?.decode()?.to_rgb8();
 
-    let image_width = img.width();
-    let image_height = img.height();
-    let y_plane_width = (image_width as f64 / 16.0).ceil() as u32 * 16;
-    let y_plane_height = (image_height as f64 / 16.0).ceil() as u32 * 16;
+    let image_width = img.width() as usize;
+    let image_height = img.height() as usize;
+    let y_plane_width = (image_width as f64 / 16.0).ceil() as usize * 16;
+    let y_plane_height = (image_height as f64 / 16.0).ceil() as usize * 16;
     let uv_plane_width = y_plane_width / 2;
     let uv_plane_height = y_plane_height / 2;
 
@@ -96,59 +95,54 @@ fn main() -> Result<()> {
         image_width, image_height, y_plane_width, y_plane_height, uv_plane_width, uv_plane_height
     );
 
-    let mut y_plane: Vec<f64> = vec![0.0; (y_plane_width * y_plane_height) as usize];
-    let mut u_plane: Vec<f64> = vec![0.0; (uv_plane_width * uv_plane_height) as usize];
-    let mut v_plane: Vec<f64> = vec![0.0; (uv_plane_width * uv_plane_height) as usize];
+    let y_dims = (y_plane_width, y_plane_height).f();
+    let uv_dims = (uv_plane_width, uv_plane_height).f();
 
-    let mut y_plane2: Vec<f64> = vec![0.0; (y_plane_width * y_plane_height) as usize];
-    let mut u_plane2: Vec<f64> = vec![0.0; (uv_plane_width * uv_plane_height) as usize];
-    let mut v_plane2: Vec<f64> = vec![0.0; (uv_plane_width * uv_plane_height) as usize];
+    let mut y_plane = Array2::<f64>::zeros(y_dims);
+    let mut u_plane = Array2::<f64>::zeros(uv_dims);
+    let mut v_plane = Array2::<f64>::zeros(uv_dims);
+
+    let mut y_plane2 = Array2::<f64>::zeros(y_dims);
+    let mut u_plane2 = Array2::<f64>::zeros(uv_dims);
+    let mut v_plane2 = Array2::<f64>::zeros(uv_dims);
 
     load_planes("data/vid/test10/001.tif", &mut y_plane, &mut u_plane, &mut v_plane)?;
     load_planes("data/vid/test10/002.tif", &mut y_plane2, &mut u_plane2, &mut v_plane2)?;
 
-    y_plane
-        .iter_mut()
-        .zip(y_plane2.iter())
-        .for_each(|(x, y)| *x = (*x - y).abs());
-    let max_val = y_plane.iter().fold(f64::NEG_INFINITY, |a, &b| a.max(b));
+    let diff = (&y_plane - &y_plane2).abs();
+    let max_val = *diff.max()?;
 
     println!("max diff: {}", max_val);
 
-    let mut result_diff = ImageBuffer::new(y_plane_width, y_plane_height);
+    let mut result_diff = ImageBuffer::new(y_plane_width as u32, y_plane_height as u32);
     for py in 0..y_plane_height {
         for px in 0..y_plane_width {
-            let y_index = (px + py * y_plane_width) as usize;
-
-            let y = y_plane[y_index];
+            let y = diff[(px, py)];
             let c = (y / max_val * 255.0).min(255.0) as u8;
 
-            result_diff.put_pixel(px, py, Luma([c]));
+            result_diff.put_pixel(px as u32, py as u32, Luma([c]));
         }
     }
 
     result_diff.save("data/result_diff.png")?;
 
-    /*let mut result_y = ImageBuffer::new(y_plane_width, y_plane_height);
-    let mut result_u = ImageBuffer::new(uv_plane_width, uv_plane_height);
-    let mut result_v = ImageBuffer::new(uv_plane_width, uv_plane_height);
-    let mut result_full = ImageBuffer::new(image_width, image_height);
+    let mut result_y = ImageBuffer::new(y_plane_width as u32, y_plane_height as u32);
+    let mut result_u = ImageBuffer::new(uv_plane_width as u32, uv_plane_height as u32);
+    let mut result_v = ImageBuffer::new(uv_plane_width as u32, uv_plane_height as u32);
+    let mut result_full = ImageBuffer::new(image_width as u32, image_height as u32);
 
     for py in 0..y_plane_height {
         for px in 0..y_plane_width {
-            let y_index = (px + py * y_plane_width) as usize;
-            let uv_index = (px / 2 + py / 2 * uv_plane_width) as usize;
-
-            let y = y_plane[y_index];
-            let u = u_plane[uv_index];
-            let v = v_plane[uv_index];
+            let y = y_plane[(px, py)];
+            let u = u_plane[(px / 2, py / 2)];
+            let v = v_plane[(px / 2, py / 2)];
             let (r, g, b) = yuv2rgb(y, u, v);
 
-            result_y.put_pixel(px, py, Luma([y as u8]));
-            result_u.put_pixel(px / 2, py / 2, Luma([u as u8]));
-            result_v.put_pixel(px / 2, py / 2, Luma([v as u8]));
+            result_y.put_pixel(px as u32, py as u32, Luma([y as u8]));
+            result_u.put_pixel(px as u32 / 2, py as u32 / 2, Luma([u as u8]));
+            result_v.put_pixel(px as u32 / 2, py as u32 / 2, Luma([v as u8]));
             if px < image_width && py < image_height {
-                result_full.put_pixel(px, py, Rgb([r, g, b]));
+                result_full.put_pixel(px as u32, py as u32, Rgb([r, g, b]));
             }
         }
     }
@@ -156,7 +150,7 @@ fn main() -> Result<()> {
     result_y.save("data/result_y.png")?;
     result_u.save("data/result_u.png")?;
     result_v.save("data/result_v.png")?;
-    result_full.save("data/result.png")?;*/
+    result_full.save("data/result.png")?;
 
     Ok(())
 }
